9609320d4ea86bc1c7f00619cbe9d565a809b7ff
NeongpoPark <parkjuyong333@gmail.com>
2022-08-05 18:20:53 +0900

    complete

---
+++ js/content.js
@@ -0,0 +1,11 @@
+const btns = document.querySelectorAll(".btns li");
+const boxs = document.querySelectorAll("section article");
+
+// 버튼의 개수만큼 반복, 해당 요소 이벤트 등록
+for(){
+    // 각 버튼 클릭
+    btns[i].addEventListener("click", () => {
+        // on 클래스 추가 및 제거 로직
+        
+    });
+};
